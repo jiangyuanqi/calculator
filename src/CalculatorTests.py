43ab603e00f6eb731e01f163ff701b703e437036
Yuanqi Jiang <yj99@njit.edu>
2019-10-26 22:16:12 -0400

    Task: add Addition

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,25 +11,18 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_property_calculator(self):
-        self.assertEqual(self.calculator.result, 0)
-
-    def test_add_method_calculator(self):
+    def test_addition(self):
         test_data = CsvReader('/src/Unit Test Addition.csv').data
         for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
+            pprint(test_data)
 
-    def test_subtraction(self):
-        test_data = CsvReader('/src/subtraction.csv').data
-        for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
-
-#        self.assertEqual(calculator.subtract(1, 1), 0)
-#       self.assertEqual(calculator.result, 0)
-
+#    def test_subtraction(self):
+#        test_data = CsvReader('/src/Unit Test Subtraction.csv').data
+#        for row in test_data:
+#            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+#            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 
