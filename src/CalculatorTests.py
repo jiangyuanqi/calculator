8d9a3ec6f571a4c74ff8c6ad4fa1e0ebff6e1534
Yuanqi Jiang <yj99@njit.edu>
2019-10-26 21:06:03 -0400

    Task: add CSV files

---
+++ src/CalculatorTests.py
@@ -0,0 +1,29 @@
+import unittest
+from Calculator import Calculator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+
+    def test_results_property_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
+    def test_subtraction(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(1, 1), 0)
+        self.assertEqual(calculator.result, 0)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
